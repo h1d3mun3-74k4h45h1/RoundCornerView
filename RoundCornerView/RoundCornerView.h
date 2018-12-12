269c66435c6bdfa46b9c4cbaf71ab8ecc55be113
Hidemune Takahashi <hidemune.takahashi@outlook.com>
2018-12-12 18:46:30 +0900

    Initial Commit

---
+++ RoundCornerView/RoundCornerView.h
@@ -0,0 +1,19 @@
+//
+//  RoundCornerView.h
+//  RoundCornerView
+//
+//  Created by hidemune on 12/12/18.
+//  Copyright © 2018 Hidemune Takahashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RoundCornerView.
+FOUNDATION_EXPORT double RoundCornerViewVersionNumber;
+
+//! Project version string for RoundCornerView.
+FOUNDATION_EXPORT const unsigned char RoundCornerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RoundCornerView/PublicHeader.h>
+
+
